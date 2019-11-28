d8cfdcbb4b56340306cab64765f84366975826fd
linze <linze@DESKTOP-9P46A3U>
2019-11-28 17:03:01 +0800

    update

--- src/com/xs/veh/network/driver/DeviceLightDriverOfMQD6A.java
+++ src/com/xs/veh/network/driver/DeviceLightDriverOfMQD6A.java
@@ -1,5 +1,6 @@
 package com.xs.veh.network.driver;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
@@ -16,6 +17,11 @@ import com.xs.veh.network.DeviceDisplay;
 import com.xs.veh.network.DeviceLight;
 import com.xs.veh.network.TakePicture;
 import com.xs.veh.network.data.LightData;
+
+import groovy.lang.GroovyObject;
+import groovy.util.GroovyScriptEngine;
+import groovy.util.ResourceException;
+import groovy.util.ScriptException;
 
 public class DeviceLightDriverOfMQD6A extends AbstractDeviceLight {
 
@@ -168,6 +174,8 @@ public class DeviceLightDriverOfMQD6A extends AbstractDeviceLight {
 	public static Integer ZT_CC = 2;
 
 	public boolean isIllegal = false;
+	
+	private VehCheckLogin vehCheckLogin;
 
 	@Override
 	public void sysSetting() {
@@ -176,7 +184,7 @@ public class DeviceLightDriverOfMQD6A extends AbstractDeviceLight {
 	@Override
 	public List<LightData> startCheck(VehCheckLogin vehCheckLogin, List<VehFlow> vheFlows)
 			throws IOException, InterruptedException, SystemException {
-
+		this.vehCheckLogin=vehCheckLogin;
 		// 设置检测参数
 		setting(vehCheckLogin, vheFlows);
 		// 等待到位
@@ -399,6 +407,7 @@ public class DeviceLightDriverOfMQD6A extends AbstractDeviceLight {
 			lightData.setGq(0);
 		} else if (CharUtil.isNumeric(gq)) {
 			lightData.setGq(Integer.parseInt(gq) * 100);
+			lightData=extend(lightData);
 		} else {
 			deviceLight.sendMessage(mtz);
 			Thread.sleep(300);
@@ -418,6 +427,26 @@ public class DeviceLightDriverOfMQD6A extends AbstractDeviceLight {
 			lightData.setDg(Integer.parseInt(dg.trim()));
 		}
 
+	}
+
+	private LightData extend(LightData lightData) {
+		File file=new File("Z:\\extend.groovy");
+		if(file.exists()) {
+			logger.info("进入扩展！");
+			try {
+				lightData.setDgpdxz(vehCheckLogin);
+				GroovyScriptEngine engine = new GroovyScriptEngine("Z:\\");
+				Class scriptClass = engine.loadScriptByName("extend.groovy");
+				GroovyObject scriptInstance = (GroovyObject)scriptClass.newInstance();
+				LightData ret = (LightData)scriptInstance.invokeMethod("extend", new Object[]{lightData});
+				return ret;
+			}catch (Exception e) {
+				logger.info("扩展类执行异常",e);
+			}
+		}
+		return lightData;
+		
+		
 	}
 
 	private boolean isEmptyData(byte[] highData) {
